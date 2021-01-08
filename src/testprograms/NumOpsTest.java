980c979403fc508f8d1eefa9361e235d8b4cfb01
fnazia <nazia.farhat@colostate.edu>
2021-01-07 23:14:34 -0700

    Added main

--- src/testprograms/NumOpsTest.java
+++ src/testprograms/NumOpsTest.java
@@ -7,25 +7,29 @@ import org.junit.Test;
 public class NumOpsTest {
 	
 	NumOps ops;
-	
+		
 	@Before
 	public void setup() {
 		ops = new NumOps(10, 5);
 	}
-	
+		
 	@Test
 	public void testAdd() {	
 		assertTrue(ops.add() == 15.0);
 	}
-	
+		
 	@Test
 	public void testDivide() {	
 		assertTrue(ops.divide() == 2.0);
 	}
-	
+		
 	@Test
 	public void testDivideFaulty() {	
 		assertTrue(ops.divide() == 3.0);
+	}
+	
+	public static void main(String[] args) {
+		
 	}
 
 }
